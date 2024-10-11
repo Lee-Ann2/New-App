7a1597fc297c5a288a7effcaca7d030e8e7af3ec
Lee-Ann2 <mfenyanakhazimla17@gmail.com>
2024-10-11 03:29:22 +0200

    Fixing changes

--- script.js
+++ script.js
@@ -1,46 +1,26 @@
-const scrollers = document.querySelectorAll('.scroller');
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-  addAnimation();
-}
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+let slider = document.querySelector('.slider');
 
-function addAnimation() {
-   scrollers.forEach((scroller) => {
-    scroller.setAttribute('data-animated', true)
-   });
-}
+next.addEventListener('click', function() {
+    let slides = document.querySelectorAll('.slides');
+    slider.appendChild(slides[0]);
+})
+prev.addEventListener('click', function() {
+    let slides = document.querySelectorAll('.slides');
+    slider.prepend(slides[slides.length - 1])
+})
 
+const next2 = document.querySelector('.next2');
+const prev2 = document.querySelector('.prev2');
+let slider2 = document.querySelector('.slider2');
 
+next2.addEventListener('click', function() {
+    let slides2 = document.querySelectorAll('.slides2');
+    slider2.appendChild(slides2[0]);
+})
+prev2.addEventListener('click', function() {
+    let slides2 = document.querySelectorAll('.slides2');
+    slider2.prepend(slides2[slides2.length - 1])
+})
 
-fetch('https://imdb188.p.rapidapi.com/api/v1/getWhatsStreaming?country=US', options)
-  .then(response => response.json())
-  .then(response => {
-    const data = response.data;
-    for (let i = 1; i <= 10; i++) {
-      console.log(data[i]);
-    }
-  })
-  .catch(error => console.error(error));
-
-  function displayProviderData(providerName) {
-    const dataObject = window.streamingData.find((obj) => obj.providerName === providerName);
-    dataObject.edges.forEach((edge) => {
-      console.log(edge); // Log each edge object
-  
-      // Access specific properties (e.g., title)
-      console.log(edge.title);
-    })
-    // Create a new window
-    const newWindow = window.open('', '_blank');
-    
-    // Create HTML content for the new window
-    const htmlContent = `
-      <h1>${providerName}</h1>
-      <ul>
-        ${dataObject.edges.map((edge) => `<li>${edge.title}</li>`).join('')}
-      </ul>
-    `;
-    
-    // Write the HTML content to the new window
-    newWindow.document.write(htmlContent);
-  }
-  
